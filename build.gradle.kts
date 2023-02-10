2b8124f0b3cec894a7dd6176010982e5647926df
Tom Olivier <contact.tom60@proton.me>
2023-02-10 17:15:04 +0100

    + Build

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,16 @@ tasks.test {
     useJUnitPlatform()
 }
 
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
